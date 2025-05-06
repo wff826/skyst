6dd5e69c3113831cca1b1d96ec7978f3cee69146
wff826 <m2njeong826@yonsei.ac.kr>
2025-05-07 08:09:21 +0900

    kakao login connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import KakaoCallback from './pages/KakaoCallback';
+import KakaoLoginButton from './components/KakaoLoginButton';
+import HomePage from './pages/HomePage'; 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<KakaoLoginButton />} />
+        <Route path="/oauth/callback/kakao" element={<KakaoCallback />} />
+        <Route path="/home" element={<HomePage />} /> {/* 로그인 성공 후 이동 경로 */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/KakaoLoginButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const KAKAO_REST_API_KEY = '90667b555f7cee12aa381be1d24a87e5';
+const REDIRECT_URI = 'http://localhost:5173/oauth/callback/kakao';
+
+const KakaoLoginButton = () => {
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>로그인 페이지</h1>
+      <a href={kakaoAuthUrl}>
+        <img 
+          src="https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_medium_narrow.png" 
+          alt="카카오 로그인" 
+        />
+      </a>
+    </div>
+  );
+};
+
+export default KakaoLoginButton;
